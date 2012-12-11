b48532df1bc65186ce76dd04aeb1d8a9dce446a4
TheRedMood <teodor_spaeren@lavabit.com>
2012-12-11 23:30:30 +0100

    I love git

---
+++ debiandeb.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install git python3 rxvt-unicode mpd nitrogen herbstluftwm \
+    luakit suckless-tools alsa-utils fontconfig xorg zsh hddtemp \
+    mesa-utils lm-sensors vim -y
